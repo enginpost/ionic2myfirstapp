b733f61b57549701e48f16fe886b78da8e36a9c7
Steve McDonald <steve.mcdonald@navitus.com>
2016-08-05 16:35:38 -0500

    added an alert

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,18 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private alertCtrl:AlertController) {
+  }
+  showAlert(){
+    let prompt = this.alertCtrl.create({
+      title: 'Greetings!',
+      message: 'Hi there!',
+      buttons: ['Dismiss']
+    });
+    prompt.present();
   }
 }
